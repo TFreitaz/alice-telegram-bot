dcde9c1a4386ceea93a112b535c71686338757be
Thales Freitas <thales.zfreitas@gmail.com>
2021-03-16 23:30:47 -0300

    Debugging

--- models/alice.py
+++ models/alice.py
@@ -100,7 +100,7 @@ class Controller:
                 self.classification = []
                 self.commands = []
                 raise e
-            zlog("Obteve ans.")
+            zlog(str(ans))
             if ans:
                 zlog("Retornou resposta.")
                 return self.send(ans)
