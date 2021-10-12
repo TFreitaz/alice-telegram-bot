65f90763c9f76723251a1f3e76161f7155e436e0
Thales <thales.zfreitas@gmail.com>
2021-10-12 11:11:27 -0300

    fix: add param name at insert() call

--- utils/user_manager.py
+++ utils/user_manager.py
@@ -126,7 +126,7 @@ class Users:
             columns = self.db.get_columns("users")
             user_data = user.to_dict()
             to_add = {col: user_data[col] for col in user_data.keys() if col in columns}
-            self.db.insert("users", list(to_add.values()), list(to_add.keys()))
+            self.db.insert("users", values=list(to_add.values()), columns=list(to_add.keys()))
             self.db.conn.close()
             return True
         return False
