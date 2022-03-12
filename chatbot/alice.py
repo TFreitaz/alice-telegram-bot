5dfd748d1e3048bda334d98ced9df5aaa91274f1
Thales <thales.zfreitas@gmail.com>
2022-03-12 19:58:25 -0300

    fix: set deafult quantity as 1

--- chatbot/alice.py
+++ chatbot/alice.py
@@ -63,6 +63,8 @@ def Help(message, **fields):
     return answers
 
 
+e
+
 # Feature functions
 
 
@@ -175,9 +177,10 @@ def purchase_registry(message, **fields):
     for s in itermsg:
         item = find_product(s.group(1).lower(), controller.user_id)
         quantity = s.group(4)
+        quantity_ef = quantity if quantity is not None else 1
         unity = s.group(6)
 
-        db.insert("purchases", values=(controller.user_id, item, quantity, unity, now))
+        db.insert("purchases", values=(controller.user_id, item, quantity_ef, unity, now))
 
         if quantity:
             if unity:
